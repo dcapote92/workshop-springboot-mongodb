09a06148048cc0f928c85d6502096efab3baea33
dcapote92 <danicapote92@gmail.com>
2025-02-15 14:38:12 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/angerbytes/workshopmongo/services/UserService.java
+++ src/main/java/com/angerbytes/workshopmongo/services/UserService.java
@@ -40,6 +40,17 @@ public class UserService {
         repo.delete(findById(id));
     }
 
+    public User update(User obj){
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save((newObj));
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
     public User fromDTO(UserDTO obj){
         return new User(obj.getId(), obj.getName(), obj.getEmail());
     }
