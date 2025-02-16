513fd5adea30f2f0b6d81edf52487fe0938ffc81
dcapote92 <danicapote92@gmail.com>
2025-02-15 21:34:38 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/angerbytes/workshopmongo/domain/Post.java
+++ src/main/java/com/angerbytes/workshopmongo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.angerbytes.workshopmongo.domain;
 
+import com.angerbytes.workshopmongo.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -19,12 +20,12 @@ public class Post {
     private Date date;
     private String title, body;
 
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -64,11 +65,11 @@ public class Post {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
