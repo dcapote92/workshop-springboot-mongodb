cf6c6ab34d1f6d0549a4661c3ba6e872efbcc803
dcapote92 <danicapote92@gmail.com>
2025-02-16 13:15:18 -0300

    Consulta de varios criterios com @Query

--- src/main/java/com/angerbytes/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/angerbytes/workshopmongo/repository/PostRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -12,6 +13,27 @@ public interface PostRepository extends MongoRepository<Post,String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
-    @Query("{'title': { $regex: ?0 , $options: 'i' }}") // ?0 is the parameter position 0,1,2,3...
+    @Query("{'title': { $regex: ?0 , $options: 'i' }}")
+        // logic -> title contains text ignoring case
+        // where 'title' is the field, $regex is the regular expression (text)
+        // ?0 is the parameter position 0,1,2,3... and 'i'  ignore case
     List<Post> searchTitle(String text);
+
+    @Query("{ $and: [" +
+                "{ date: { $gte: ?1 } }," +
+                "{ date: { $lte: ?2 } }," +
+                "{ $or: [" +
+                    "{ title: { $regex: ?0, $options: 'i' } }," +
+                    "{ body: { $regex: ?0, $options: 'i' } }," +
+                    "{ 'comments.text': { $regex: ?0, $options: 'i' } }" +
+                 "]}" +
+            "]}")
+        // logic -> (
+        //      date >= minDate && date <= maxDate &&
+        //      (title contains text || body contains text || comments.text contains text)
+        //      )
+        // where $and == && , $or == || , $gte == greater than or equal , $lte == less than or equal
+
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
+
 }
