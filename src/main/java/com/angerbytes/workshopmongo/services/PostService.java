5dae0fe69fcfbcd7d26549bd8ca0e4cfed579143
dcapote92 <danicapote92@gmail.com>
2025-02-15 21:57:16 -0300

    Obtendo post por id

---
+++ src/main/java/com/angerbytes/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.angerbytes.workshopmongo.services;
+
+import com.angerbytes.workshopmongo.domain.Post;
+import com.angerbytes.workshopmongo.repository.PostRepository;
+import com.sun.jdi.ObjectCollectedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id){
+        Optional<Post> post = repo.findById(id);
+        return post.orElseThrow(() -> new ObjectCollectedException("Objeto não encontrado"));
+    }
+
+
+
+
+}
